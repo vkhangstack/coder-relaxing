b053fbadba174edf81e2705e5ad5955eb783fd51
Pham Van Khang <phamvankhang.tvi@gmail.com>
2023-05-26 00:32:35 +0700

    ao that day

--- database/db.js
+++ database/db.js
@@ -6,8 +6,8 @@ module.exports = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     };
-    await mongoose.connect(
-      process.env.MONGO_URL || "mongodb://localhost:27017/girls", // mongo cloud or mongodb on local
+    mongoose.connect(
+      process.env.MONGO_URL || "mongodb://localhost:27017/girls",
       connectionParams
     );
     // eslint-disable-next-line no-console
--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const connectionDB = require('./database/db');
-const girlsCute = require('./routes/girlsCute');
-const girlsSexy = require('./routes/girlsSexy');
-const contribute = require('./routes/contribute');
-const path = require('path');
-const key = require('./routes/privateKey');
+const connectionDB = require("./database/db");
+const girlsCute = require("./routes/girlsCute");
+const girlsSexy = require("./routes/girlsSexy");
+const contribute = require("./routes/contribute");
+const path = require("path");
+const key = require("./routes/privateKey");
 
 connectionDB();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/girls', girlsCute);
-app.use('/girls', girlsSexy);
-app.use('/girls', contribute);
-app.use('/girls', key);
-app.get('/', (_req, res) => res.redirect('/girls/cute'));
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/girls", girlsCute);
+app.use("/girls", girlsSexy);
+app.use("/girls", contribute);
+app.use("/girls", key);
+app.get("/", (_req, res) => res.redirect("/girls/cute"));
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Server on listening port ${PORT}...`);
